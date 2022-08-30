649e3ec954cbd1ef8ab5b2c570dc1710ead521b5
ttasjwi <ttasjwi920@gmail.com>
2022-08-30 15:16:19 +0900

    Initial commit

---
+++ src/main/java/com/ttasjwi/advanced/AdvancedApplication.java
@@ -0,0 +1,13 @@
+package com.ttasjwi.advanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ttasjwi/advanced/AdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttasjwi.advanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
