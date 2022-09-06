130e05ae619c83a34802625dd5aed02ca2bc89be
ttasjwi <ttasjwi920@gmail.com>
2022-09-06 20:51:01 +0900

    로그 추적기 V2 - 파라미터로 동기화 개발
    - 기존 트랜잭션 Id를 이어가기 위해, TraceId를 파라미터로 삼아 이어서 추적하는 메서드를 추가

---
+++ src/test/java/com/ttasjwi/advanced/trace/hellotrace/HelloTraceV2Test.java
@@ -0,0 +1,31 @@
+package com.ttasjwi.advanced.trace.hellotrace;
+
+import com.ttasjwi.advanced.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+
+class HelloTraceV2Test {
+
+    @Test
+    void begin_end() {
+        HelloTraceV2 trace = new HelloTraceV2();
+
+        TraceStatus status1 = trace.begin("hello1");
+
+        TraceStatus status2 = trace.beginSync(status1.getTraceId(), "hello2");
+        trace.end(status2);
+
+        trace.end(status1);
+    }
+
+    @Test
+    public void begin_exception() {
+        HelloTraceV2 trace = new HelloTraceV2();
+
+        TraceStatus status1 = trace.begin("hello1");
+
+        TraceStatus status2 = trace.beginSync(status1.getTraceId(), "hello2");
+        trace.exception(status2, new IllegalArgumentException());
+
+        trace.exception(status1, new IllegalArgumentException());
+    }
+}
