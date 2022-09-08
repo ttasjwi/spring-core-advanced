afa2dd75734b91eb3701247b5c1b60e1495d8773
ttasjwi <ttasjwi920@gmail.com>
2022-09-08 18:40:29 +0900

    스레드 로컬 동기화 - 적용
    - LogTrace 구현체를 `ThreadLocalLogTrace`로 변경했다.
    - 웹 요청 시 로그 추적 시, 동시성 이슈를 해결했다.

--- src/main/java/com/ttasjwi/advanced/LogTraceConfig.java
+++ src/main/java/com/ttasjwi/advanced/LogTraceConfig.java
@@ -1,7 +1,7 @@
 package com.ttasjwi.advanced;
 
-import com.ttasjwi.advanced.trace.logtrace.FieldLogTrace;
 import com.ttasjwi.advanced.trace.logtrace.LogTrace;
+import com.ttasjwi.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +10,7 @@ public class LogTraceConfig {
 
     @Bean
     public LogTrace logTrace() {
-        return new FieldLogTrace();
+        //return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 }
