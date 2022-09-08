0f4a942c3cc27b7f22ac5e4d13ab298e4e38d896
ttasjwi <ttasjwi920@gmail.com>
2022-09-08 19:30:18 +0900

    템플릿 메서드 패턴 - 예제2
    - 템플릿 메서드 패턴은 부모 클래스에 변하지 않는 템플릿 코드를 둔다. 그리고 변하는 부분은 자식 클래스에 두고 상속과 오버라이딩을 사용해서 처리한다.

--- src/test/java/com/ttasjwi/advanced/trace/template/TemplateMethodTest.java
+++ src/test/java/com/ttasjwi/advanced/trace/template/TemplateMethodTest.java
@@ -1,5 +1,8 @@
 package com.ttasjwi.advanced.trace.template;
 
+import com.ttasjwi.advanced.trace.template.code.AbstractTemplate;
+import com.ttasjwi.advanced.trace.template.code.SubClassLogic1;
+import com.ttasjwi.advanced.trace.template.code.SubClassLogic2;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -10,6 +13,18 @@ public class TemplateMethodTest {
     void templateMethodV0() {
         logic1();
         logic2();
+    }
+
+    /**
+     * 템플릿 메서드 패턴 적용
+     */
+    @Test
+    void TemplateMethodV1() {
+        AbstractTemplate template1 = new SubClassLogic1();
+        template1.execute();
+
+        AbstractTemplate template2 = new SubClassLogic2();
+        template2.execute();
     }
 
     private void logic1() {
