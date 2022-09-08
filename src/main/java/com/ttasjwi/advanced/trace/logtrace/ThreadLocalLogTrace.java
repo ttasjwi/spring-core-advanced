b661310b659f814255c80ae2fb7d6a98c6dbd4c8
ttasjwi <ttasjwi920@gmail.com>
2022-09-08 17:57:17 +0900

    스레드 로컬 동기화 - 개발
    - 스레드 로컬을 적용한 로그 추적기를 개발했다.
    - 해당 스레드에서 작업을 마치면 스레드 로컬에 대해서 remove를 호출해 해당 스레드 데이터를 제거해야한다.

---
+++ src/main/java/com/ttasjwi/advanced/trace/logtrace/ThreadLocalLogTrace.java
@@ -0,0 +1,78 @@
+package com.ttasjwi.advanced.trace.logtrace;
+
+import com.ttasjwi.advanced.trace.TraceId;
+import com.ttasjwi.advanced.trace.TraceStatus;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ThreadLocalLogTrace implements LogTrace {
+
+    private static final String START_PREFIX = "-->";
+    private static final String COMPLETE_PREFIX = "<--";
+    private static final String EX_PREFIX = "<X-";
+
+    private ThreadLocal<TraceId> traceIdHolder = new ThreadLocal<>(); // traceId 동기화, 동시성 이슈 발생
+
+    @Override
+    public TraceStatus begin(String message) {
+        syncTraceId();
+        Long startTimeMs = System.currentTimeMillis();
+
+        TraceId traceId = traceIdHolder.get();
+
+        log.info("[{}] {}{}", traceId.getId(), addSpace(START_PREFIX, traceId.getLevel()), message);
+        return new TraceStatus(traceId, startTimeMs, message);
+    }
+
+    private void syncTraceId() {
+        TraceId traceId= traceIdHolder.get();
+        if (traceId == null) {
+            traceIdHolder.set(new TraceId());
+        } else {
+            traceIdHolder.set(traceId.createNextId());
+        }
+    }
+
+    @Override
+    public void end(TraceStatus status) {
+        complete(status, null);
+    }
+
+    @Override
+    public void exception(TraceStatus status, Exception e) {
+        complete(status, e);
+    }
+
+    // 로그를 찍을 때 메서드 깊이를 나타나는데 사용
+    private static String addSpace(String prefix, int level) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < level; i++) {
+            sb.append((i == level - 1) ? "|" + prefix : "| ");
+        }
+        return sb.toString();
+    }
+
+    // end, exception의 로직을 하나의 메서드에서 처리
+    // 총 실행시간을 측정하고 로그를 남긴다.
+    private void complete(TraceStatus status, Exception e) {
+        Long stopTimeMs = System.currentTimeMillis();
+        long resultTimeMs = stopTimeMs - status.getStartTimeMs();
+        TraceId traceId = status.getTraceId();
+        if (e == null) {
+            log.info("[{}] {}{} time={}ms", traceId.getId(), addSpace(COMPLETE_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs);
+        } else {
+            log.info("[{}] {}{} time={}ms ex={}", traceId.getId(), addSpace(EX_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs, e.toString());
+        }
+
+        releaseTraceId();
+    }
+
+    private void releaseTraceId() {
+        TraceId traceId = traceIdHolder.get();
+        if (traceId.isFirstLevel()) {
+            traceIdHolder.remove(); // 해당 스레드의 전용 보관소 destroy
+        } else {
+            traceIdHolder.set(traceId.createPreviousId()); // 이전 메서드 스택 프레임으로 돌아가기 직전, 레벨 감소
+        }
+    }
+}
