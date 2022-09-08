5374a1ddae76a8731f915a1b96d2a6dd75694d4f
ttasjwi <ttasjwi920@gmail.com>
2022-09-08 21:04:25 +0900

    전략 패턴 - 예제2
    - 익명 클래스 방식
    - 익명 클래스를 인자로 통채로 넘기는 방식
    - 람다를 사용하는 방식

--- src/test/java/com/ttasjwi/advanced/trace/strategy/ContextV1Test.java
+++ src/test/java/com/ttasjwi/advanced/trace/strategy/ContextV1Test.java
@@ -31,6 +31,57 @@ public class ContextV1Test {
         context2.execute();
     }
 
+    @Test
+    void strategyV2() {
+        Strategy strategyLogic1 = new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+        log.info("strategyLogic1 = {}", strategyLogic1.getClass());
+        ContextV1 contextV1 = new ContextV1(strategyLogic1);
+        contextV1.execute();
+
+        Strategy strategyLogic2 = new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        };
+        log.info("strategyLogic2 = {}", strategyLogic2.getClass());
+        ContextV1 contextV2 = new ContextV1(strategyLogic2);
+        contextV2.execute();
+    }
+
+    @Test
+    void strategyV3() {
+        ContextV1 contextV1 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        });
+        contextV1.execute();
+
+        ContextV1 contextV2 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        });
+        contextV2.execute();
+    }
+
+    @Test
+    void strategyV4() {
+        ContextV1 contextV1 = new ContextV1(() -> log.info("비즈니스 로직1 실행"));
+        contextV1.execute();
+
+        ContextV1 contextV2 = new ContextV1(() -> log.info("비즈니스 로직2 실행"));
+        contextV2.execute();
+    }
+
     private void logic1() {
         long startTime = System.currentTimeMillis();
         // 비즈니스 로직 실행
