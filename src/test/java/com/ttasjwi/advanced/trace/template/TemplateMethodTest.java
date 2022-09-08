8d8961a62027a932c69509d6baa079e2a21976ee
ttasjwi <ttasjwi920@gmail.com>
2022-09-08 19:36:54 +0900

    템플릿 메서드 패턴 - 예제3
    - 템플릿 메서드 패턴을 익명 클래스를 이용해 적용함
    - 템플릿 메서드 패턴은 SubClassLogic1 , SubClassLogic2 처럼 클래스를 계속 만들어야 하는 단점이 있다.
    - 익명 내부 클래스를 사용하면 이런 단점을 보완할 수 있다.
    - 익명 내부 클래스를 사용하면 객체 인스턴스를 생성하면서 동시에 생성할 클래스를 상속 받은 자식 클래스를 정의할 수 있다.

--- src/test/java/com/ttasjwi/advanced/trace/template/TemplateMethodTest.java
+++ src/test/java/com/ttasjwi/advanced/trace/template/TemplateMethodTest.java
@@ -19,11 +19,32 @@ public class TemplateMethodTest {
      * 템플릿 메서드 패턴 적용
      */
     @Test
-    void TemplateMethodV1() {
+    void templateMethodV1() {
         AbstractTemplate template1 = new SubClassLogic1();
         template1.execute();
 
         AbstractTemplate template2 = new SubClassLogic2();
+        template2.execute();
+    }
+
+    @Test
+    void templateMethodV2() {
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+        log.info("클래스 이름1 = {}", template1.getClass());
+        template1.execute();
+
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        };
+        log.info("클래스 이름2 = {}", template2.getClass());
         template2.execute();
     }
 
