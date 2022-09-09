28669f0cdbf003c5eeed20abbd94c7f61f17bc3a
ttasjwi <ttasjwi920@gmail.com>
2022-09-09 13:37:00 +0900

    로그 공백 처리 수정

--- src/main/java/com/ttasjwi/advanced/trace/logtrace/FieldLogTrace.java
+++ src/main/java/com/ttasjwi/advanced/trace/logtrace/FieldLogTrace.java
@@ -46,7 +46,7 @@ public class FieldLogTrace implements LogTrace {
     private static String addSpace(String prefix, int level) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < level; i++) {
-            sb.append((i == level - 1) ? "|" + prefix : "| ");
+            sb.append((i == level - 1) ? "|" + prefix : "|   ");
         }
         return sb.toString();
     }
--- src/main/java/com/ttasjwi/advanced/trace/logtrace/ThreadLocalLogTrace.java
+++ src/main/java/com/ttasjwi/advanced/trace/logtrace/ThreadLocalLogTrace.java
@@ -47,7 +47,7 @@ public class ThreadLocalLogTrace implements LogTrace {
     private static String addSpace(String prefix, int level) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < level; i++) {
-            sb.append((i == level - 1) ? "|" + prefix : "| ");
+            sb.append((i == level - 1) ? "|" + prefix : "|   ");
         }
         return sb.toString();
     }
